18ae22b3c6f367bd4544f7a6abb423ec0839b3c5
Jacek Kosiński <kashiash@gmail.com>
2022-12-25 10:54:57 +0100

    Initial Commit

---
+++ LocationSearchApp/LocationSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationSearchAppApp.swift
+//  LocationSearchApp
+//
+//  Created by Jacek Kosinski U on 25/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct LocationSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
